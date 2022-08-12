feb797da7511dc86b478f81376a5a0ae5e25e587
Monika Feldmann <mof@lomrh442.meteoswiss.ch>
2022-08-12 14:16:35 +0000

    plotting with thunderstorm areas and precipitation rate

--- realtime_plot.py
+++ realtime_plot.py
@@ -20,12 +20,14 @@ import sys
 sys.path.append('/users/mfeldman/scripts/ELDES_MESO')
 sys.path.append('/scratch/lom/mof/code/ELDES_MESO')
 import pandas as pd
+import skimage.morphology as skim
 pd.options.mode.chained_assignment = None
 import warnings
 from astropy.utils.exceptions import AstropyWarning
 warnings.simplefilter('ignore',category=AstropyWarning)
 import library.variables as variables
 import library.plot as plot
+import library.io as io
 import glob
 import pyart
 import geojson as gs
@@ -67,12 +69,20 @@ if len(vert_n)==0: vert_n=pd.DataFrame(columns=['geometry', 'ID', 'time', 'x', '
        'rank_100', 'rank_IQR', 'rank_mean', 'cont', 'dist', 'flag'])
 
 #%%
-b_file=glob.glob(path["lomdata"]+'BZC/*'+str(time)+'*')
+b_file=glob.glob(path["lomdata"]+'RZC/*'+str(time)+'*')[0]
+print(b_file)
 #b_file=glob.glob(path["lomdata"]+'LZC/*'+str(time)+'*')
-metranet=pyart.aux_io.read_cartesian_metranet(b_file[0])
-background=metranet.fields['probability_of_hail']['data'][0,:,:]
+metranet=pyart.aux_io.read_cartesian_metranet(b_file,reader='python')
+czc=metranet.fields['radar_estimated_rain_rate']['data'][0,:,:]
+cells,timelist=io.get_TRT(time, path)
+#print(np.unique(czc))
+newcells=skim.dilation(cells[0],footprint=np.ones([5,5]))
+newcells[newcells==0]=np.nan
+newcells[newcells>0]=1
+background=newcells*czc
 #background=metranet.fields['vertically_integrated_liquid']['data'][0,:,:]
 xp=vert_p.x; yp=vert_p.y; sp=np.nansum([vert_p.A_n,vert_p.D_n,vert_p.L_n,vert_p.P_n,vert_p.W_n]);fp=vert_p.flag;cp=vert_p.rank_90
 xn=vert_n.x; yn=vert_n.y; sn=np.nansum([vert_n.A_n,vert_n.D_n,vert_n.L_n,vert_n.P_n,vert_n.W_n]);fn=vert_n.flag;cn=vert_n.rank_90
 imtitle='Detected mesocyclones on VIL background';savepath=path["outdir"]+'IM/';imname='ROT'+str(time+'.png')
+print(len(xp),len(xn))
 plot.plot_cart_obj(background, xp, yp, sp*20, fp, xn, yn, sn*20, fn, cp, cn, imtitle, savepath, imname, radar)
