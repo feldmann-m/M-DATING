661e48c586e033ecdc62d24dab3fe7a5ed7fff86
feldmann <monika.feldmann@epfl.ch>
2023-03-29 15:28:36 +0200

    rename files

--- prev_plot.py
+++ prev_plot.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb  1 16:04:19 2023
+Created on Mon Jul 25 16:24:04 2022
 
 @author: mfeldman
 
-Creates plots for radarlive from realtime production, includes history of past 2h
+Creates plots for radarlive from realtime production
 run after realtime_parallel.py
 """
-#%% import settings
+#%%Import settings
 import argparse as ap
 parser = ap.ArgumentParser()
 parser.add_argument('--dvdir', type=str, required=False,default='/srn/data/zuerh450/')
@@ -17,7 +17,8 @@ parser.add_argument('--outdir', type=str, required=False,default='/scratch/lom/m
 parser.add_argument('--codedir', type=str, required=False,default='/scratch/lom/mof/code/ELDES_MESO/')
 parser.add_argument('--time', type=str, required=True)
 args = parser.parse_args()
-#%% import external libraries
+#%%Import external libraries
+import numpy as np
 import sys
 sys.path.append(args.codedir)
 import os
@@ -28,48 +29,28 @@ pd.options.mode.chained_assignment = None
 import warnings
 from astropy.utils.exceptions import AstropyWarning
 warnings.simplefilter('ignore',category=AstropyWarning)
-import glob
-import numpy as np
 import geojson as gs
 import geopandas as gpd
 from geojson import FeatureCollection
+import glob
 import pyart
-#%% import functions
+#%%Import functions
 import library.variables as variables
 import library.plot as plot
 import library.io as io
-#%% Main function
+
+
+#%%
 def main():
-    #import variables
+    #import radar variables
     time=args.time
-    
     radar, cartesian, path, specs, files, shear, resolution=variables.vars(args.dvdir,args.lomdir,args.outdir,args.codedir)
-    #find TRT and rotation files of given day
-    trtfiles=np.array(sorted(glob.glob(path["lomdata"]+'TRTC/*.json')))
-    trtfile=np.array(sorted(glob.glob(path["lomdata"]+'TRTC/*'+time+'*.json')))
-    pfiles=np.array(sorted(glob.glob(path["outdir"]+'ROT/'+'PROT*.json')))
-    pfile=np.array(sorted(glob.glob(path["outdir"]+'ROT/'+'PROT*'+time+'*.json')))
-    nfiles=np.array(sorted(glob.glob(path["outdir"]+'ROT/'+'NROT*.json')))
-    nfile=np.array(sorted(glob.glob(path["outdir"]+'ROT/'+'NROT*'+time+'*.json')))
-    i=np.where(trtfiles==trtfile)[0][0].astype(int)+1
-    ii=np.where(pfiles==pfile)[0][0].astype(int)+1
-    iii=np.where(nfiles==nfile)[0][0].astype(int)+1
-    if np.nanmin([i,ii,iii])<12:
-        trtfiles=trtfiles[:i]
-        pfiles=pfiles[:ii]
-        nfiles=nfiles[:iii]
-    else:
-        trtfiles=trtfiles[i-12:i]
-        pfiles=pfiles[ii-12:ii]
-        nfiles=nfiles[iii-12:iii]
-    # pfiles=glob.glob(path["outdir"]+'ROT/'+'PROT*'+day+'*.json')
-    # pfiles=sorted(pfiles)
-    # nfiles=glob.glob(path["outdir"]+'ROT/'+'NROT*'+day+'*.json')
-    # nfiles=sorted(nfiles)
     
-    #%%initialize empty dataframes to append
-    trtcells=pd.DataFrame()
-    vert_p=pd.DataFrame(columns=['geometry', 'ID', 'time', 'x', 'y', 'dz', 'A', 'D', 'L', 'P', 'W',
+    #%%read rotation files of according timestep
+    pfile=path["outdir"]+'ROT/'+'PROT'+str(time)+'.json'
+    with open(pfile) as f: gj = FeatureCollection(gs.load(f))
+    vert_p=gpd.GeoDataFrame.from_features(gj['features'])
+    if len(vert_p)==0: vert_p=pd.DataFrame(columns=['geometry', 'ID', 'time', 'x', 'y', 'dz', 'A', 'D', 'L', 'P', 'W',
            'A_range', 'D_range', 'L_range', 'P_range', 'W_range', 'A_n', 'D_n',
            'L_n', 'P_n', 'W_n', 'A_el', 'D_el', 'L_el', 'P_el', 'W_el', 'size_sum',
            'size_mean', 'vol_sum', 'vol_mean', 'z_0', 'z_10', 'z_25', 'z_50',
@@ -79,7 +60,10 @@ def main():
            'd_10', 'd_25', 'd_50', 'd_75', 'd_90', 'd_100', 'd_IQR', 'd_mean',
            'rank_0', 'rank_10', 'rank_25', 'rank_50', 'rank_75', 'rank_90',
            'rank_100', 'rank_IQR', 'rank_mean', 'cont', 'dist', 'flag'])
-    vert_n=pd.DataFrame(columns=['geometry', 'ID', 'time', 'x', 'y', 'dz', 'A', 'D', 'L', 'P', 'W',
+    nfile=path["outdir"]+'ROT/'+'NROT'+str(time)+'.json'
+    with open(nfile) as f: gj = FeatureCollection(gs.load(f))
+    vert_n=gpd.GeoDataFrame.from_features(gj['features'])
+    if len(vert_n)==0: vert_n=pd.DataFrame(columns=['geometry', 'ID', 'time', 'x', 'y', 'dz', 'A', 'D', 'L', 'P', 'W',
            'A_range', 'D_range', 'L_range', 'P_range', 'W_range', 'A_n', 'D_n',
            'L_n', 'P_n', 'W_n', 'A_el', 'D_el', 'L_el', 'P_el', 'W_el', 'size_sum',
            'size_mean', 'vol_sum', 'vol_mean', 'z_0', 'z_10', 'z_25', 'z_50',
@@ -89,36 +73,21 @@ def main():
            'd_10', 'd_25', 'd_50', 'd_75', 'd_90', 'd_100', 'd_IQR', 'd_mean',
            'rank_0', 'rank_10', 'rank_25', 'rank_50', 'rank_75', 'rank_90',
            'rank_100', 'rank_IQR', 'rank_mean', 'cont', 'dist', 'flag'])
-    #read TRT and rotation files of day and add to dataframes
-    for file in trtfiles:
-        print(file)
-        tdat,tcells,timelist=io.read_TRT(path,file=file)
-        trtcells=pd.concat((trtcells,tdat),axis=0)#trtcells.append(tdat)
-    for nfile in nfiles:
-        with open(nfile) as f: gj = FeatureCollection(gs.load(f))
-        vert_n=pd.concat((vert_n,gpd.GeoDataFrame.from_features(gj['features'])),axis=0)#vert_n.append(gpd.GeoDataFrame.from_features(gj['features']))
-        
-    for pfile in pfiles:
-        with open(pfile) as f: gj = FeatureCollection(gs.load(f))
-        vert_p=pd.concat((vert_p,gpd.GeoDataFrame.from_features(gj['features'])),axis=0)#vert_p.append(gpd.GeoDataFrame.from_features(gj['features']))
-    print(vert_p); print(vert_n); print(trtcells)
+    
     #%%read TRT file of timestep, use to cut out precipitation data -> displayed in background
     cells,timelist=io.get_TRT(time, path)
     if len(cells)>0:
       try:
-        b_file=glob.glob(path["lomdata"]+'CZC/*'+str(time)+'*')[0]
+        b_file=glob.glob(path["lomdata"]+'RZC/*'+str(time)+'*')[0]
         print(b_file)
         metranet=pyart.aux_io.read_cartesian_metranet(b_file)
-        czc=metranet.fields['maximum_echo']['data'][0,:,:]
-        #newcells=skim.dilation(cells[0],footprint=np.ones([5,5]))
-        #newcells[newcells==0]=np.nan
-        #newcells[newcells>0]=1
-        #background=newcells*czc
-        import copy
-        background=copy.deepcopy(czc)
-        background[background<35]=np.nan
+        czc=metranet.fields['radar_estimated_rain_rate']['data'][0,:,:]
+        newcells=skim.dilation(cells[0],footprint=np.ones([5,5]))
+        newcells[newcells==0]=np.nan
+        newcells[newcells>0]=1
+        background=newcells*czc
       except:
-        b_file=glob.glob(path["lomdata"]+'CZC/*'+str(time)+'*')[0]
+        b_file=glob.glob(path["lomdata"]+'RZC/*'+str(time)+'*')[0]
         print('Problem with file',b_file)
         newcells=skim.dilation(cells[0],footprint=np.ones([5,5]))
         newcells[newcells==0]=np.nan
@@ -128,9 +97,13 @@ def main():
     else:
       background=np.zeros([640,710])
       background[:]=np.nan
-    #%% generate plot
+    #extract mesocyclone metrics
+    xp=vert_p.x; yp=vert_p.y; sp=np.nansum([vert_p.A_n,vert_p.D_n,vert_p.L_n,vert_p.P_n,vert_p.W_n]);fp=vert_p.flag;cp=vert_p.rank_90
+    xn=vert_n.x; yn=vert_n.y; sn=np.nansum([vert_n.A_n,vert_n.D_n,vert_n.L_n,vert_n.P_n,vert_n.W_n]);fn=vert_n.flag;cn=vert_n.rank_90
     imtitle='Detected mesocyclones on VIL background';savepath=path["outdir"]+'IM/';imname='ROT'+str(time+'.png')
-    plot.plot_cart_hist(time,background,trtcells,vert_p,vert_n, imtitle, savepath, imname, radar)
+    print(len(xp),len(xn))
+    #generate plot
+    plot.plot_cart_obj(background, xp, yp, sp*20, fp, xn, yn, sn*20, fn, cp, cn, imtitle, savepath, imname, radar)
 
 #%% CALL MAIN FUNCTION
 
