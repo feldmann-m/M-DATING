a3886c490d7fcbfec5b575828e1520cbe7a376bc
Simone Balmelli <simone.balmelli@meteoswiss.ch>
2025-01-20 15:17:21 +0100

    Correct loop over radars

--- realtime_parallel.py
+++ realtime_parallel.py
@@ -206,14 +206,13 @@ def radel_processor (rotation_pos, rotation_neg, rels, radar, cartesian, path, s
     # Initialize rotation dictionaries
     rotation_pos1=variables.meso()
     rotation_neg1=variables.meso()
-    nn=0
     for rel in rels:
       print('Radar-elevation is', rel)
       # Regain radar and elevation numbers
       r=int(rel/100)-1
       el=rel%100-1
       print("Analysing radar: ",r+1,", elevation: ",el+1)
-      l_mask=masks[nn]
+      l_mask=masks[r]
     
       # Find and read velocity file
       dvfile=glob.glob(path["dvdata"]+'DV'+radar["radars"][r]+'/*'+timelist[t]+'*.8'+radar["elevations"][el])[0]
@@ -238,7 +237,6 @@ def radel_processor (rotation_pos, rotation_neg, rels, radar, cartesian, path, s
           # process each thunderstorm individually
           for ii in ids:
               rotation_pos1, rotation_neg1 = meso.cell_loop(ii, l_mask, az_shear, mfd_conv, rotation_pos1, rotation_neg1, distance, resolution, shear, radar, coord, timelist, r, el)
-          nn+=1 
           rotation_pos["prop"]=pd.concat([rotation_pos["prop"],rotation_pos1["prop"]], ignore_index=True)
           rotation_neg["prop"]=pd.concat([rotation_neg["prop"],rotation_neg1["prop"]], ignore_index=True) 
           rotation_pos["shear_objects"].append(rotation_pos1["shear_objects"])
